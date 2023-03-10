b9ea161fb5dda04071b06c63f455189c9e4bbe2f
flynn-ng <ng.flynn.phong@gmail.com>
2023-03-10 15:47:49 +0700

    add profile address field

--- lib/src/features/profile/domain/profile.dart
+++ lib/src/features/profile/domain/profile.dart
@@ -6,21 +6,24 @@ class Profile {
     this.lastName,
     this.gender,
     this.bio,
+    this.address,
   });
-  final String username;
+  final String? username;
   // final String imageUrl;
   final String? phoneNumber;
   final String? firstName;
   final String? lastName;
   final String? gender;
   final String? bio;
+  final String? address;
   factory Profile.fromJson(Map<String, dynamic> data) {
-    final username = data['username'] as String;
+    final username = data['username'] as String?;
     final phoneNumber = data['telephoneNumber'] as String?;
     final firstName = data['firstName'] as String?;
     final lastName = data['lastName'] as String?;
     final gender = data['gender'] as String?;
     final bio = data['bio'] as String?;
+    final address = data['address'] as String?;
     return Profile(
       username: username,
       phoneNumber: phoneNumber,
@@ -28,6 +31,7 @@ class Profile {
       lastName: lastName,
       gender: gender,
       bio: bio,
+      address: address,
     );
   }
   Map<String, dynamic> toJson() {
@@ -38,6 +42,7 @@ class Profile {
       'lastName': lastName,
       'gender': gender,
       'bio': bio,
+      'address': address,
     };
   }
 }
