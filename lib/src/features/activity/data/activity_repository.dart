c4792c323c13cd6655d865eb5689e0a31a6870bc
flynn-ng <ng.flynn.phong@gmail.com>
2023-04-17 03:25:16 +0700

    add activity repository

---
+++ lib/src/features/activity/data/activity_repository.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+class ActivityRepository {
+  ActivityRepository({required this.dio});
+  final Dio dio;
+  // TODO
+  // Future<List<Activity>> fetchActivities({CancelToken? cancelToken}) {
+  //   return UnimplementedError();
+  // }
+  // TODO
+  // Future<Activity> fetchActivity(int activityId, {CancelToken? cancelToken}) {
+  //   return UnimplementedError();
+  // }
+}
---
+++ lib/src/features/activity/domain/activity.dart
@@ -0,0 +1,27 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:the_helper/src/features/location/domain/location.dart';
+
+part 'activity.g.dart';
+part 'activity.freezed.dart';
+
+@freezed
+class Activity with _$Activity {
+  const factory Activity({
+    required String id,
+    required String name,
+    String? description,
+    // TODO: replace int with actual model
+    int? thumbnail,
+    int? organizationId,
+    List<int>? activityTypeIds,
+    List<int>? activityManagerIds,
+    DateTime startTime,
+    DateTime endTime,
+    Location location,
+    int? maxParticipants,
+    int? joinedParticipants,
+  }) = _Activity;
+  factory Activity.fromJson(Map<String, dynamic> json) =>
+      _$ActivityFromJson(json);
+}
