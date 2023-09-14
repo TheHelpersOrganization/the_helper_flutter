58beff71e461ab75ab9b1e3e15a753644e3e1be9
DuyVo09 <duyvoduc09@gmail.com>
2023-09-14 21:15:10 +0700

    fix mod news list

--- lib/src/features/news/presentation/news_list/screen/news_list_mod_screen.dart
+++ lib/src/features/news/presentation/news_list/screen/news_list_mod_screen.dart
@@ -4,6 +4,8 @@ import 'package:the_helper/src/common/widget/drawer/app_drawer.dart';
 import 'package:the_helper/src/features/news/presentation/news_list/widget/moderator/app_bar/organization_news_list_app_bar.dart';
 import 'package:the_helper/src/features/news/presentation/news_list/widget/moderator/organization_news_list.dart';
 import 'package:the_helper/src/router/router.dart';
+import 'package:the_helper/src/features/news/presentation/news_list/widget/moderator/app_bar/news_status_filter.dart';
+import 'package:the_helper/src/features/news/presentation/news_list/widget/moderator/app_bar/news_type_filter.dart';
 
 class NewsListModScreen extends StatelessWidget {
   const NewsListModScreen({super.key});
@@ -15,6 +17,19 @@ class NewsListModScreen extends StatelessWidget {
       body: const CustomScrollView(
         slivers: [
           OrganizationNewsListAppBar(),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  NewsStatusFilter(),
+                  SizedBox(height: 8),
+                  NewsTypeFilter(),
+                ],
+              ),
+            ),
+          ),
           SliverPadding(
             padding: EdgeInsets.all(12),
             sliver: OrganizationNewsList(),
